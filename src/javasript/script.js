41972203606d46f1718ca492f196b5a19512147e
joaovictor <joaovictorfp08@gmail.com>
2024-07-10 20:20:55 -0300

    10/07/24

--- src/javasript/script.js
+++ src/javasript/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('#mobile_btn').on('click', function() {
-       $('#mobile_menu').toggLeClass('active')
-    })
+       $('#mobile_menu').toggLeclass('active')
+       $('#mobile_menu').find('i').toggLeclass('fa-x')
+    });
 });
